075cbfb43714fe032a57798310cdd62ffc1552b1
tatmanblue <Matt.Raffel@gmail.com>
2021-09-25 12:45:59 -0600

    adds new file
    Signed-off-by: tatmanblue <Matt.Raffel@gmail.com>

---
+++ game/Assets/My Assets/Code/Consumerism/Interfaces.cs
@@ -0,0 +1,14 @@
+namespace F500.Consumerism
+{
+	// Producers add "items" into the economy
+	public interface IProducer {}
+	
+	// Consumers remove "items" from the economy
+	public interface IConsumer {}
+	
+	// Sellers sell items to the market
+	public interface ISeller {}
+	
+	// Buyers buy items from the market
+	public interface IBuyer {}
+}
